4321e68eadc67ab4a6c7ea12b47789cf168f4b8d
Makovei Miron <miron.makovei@gmail.com>
2024-09-14 19:02:20 +0200

    on reload scroll to top

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 // Smooth scroll for internal links
+
+window.onload = function () {
+    window.scrollTo(0, 0);
+};
+
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     anchor.addEventListener('click', function(e) {
         e.preventDefault();
