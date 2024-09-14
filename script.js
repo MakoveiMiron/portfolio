e3312c2ceaabafeb27cbb2ac180f95a6726a7133
Makovei Miron <miron.makovei@gmail.com>
2024-09-14 19:14:39 +0200

    dd

--- script.js
+++ script.js
@@ -1,9 +1,5 @@
 // Smooth scroll for internal links
 
-window.onload = function () {
-    window.scrollTo(0, 0);
-};
-
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     anchor.addEventListener('click', function(e) {
         e.preventDefault();
