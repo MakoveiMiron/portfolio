16000f2abd96db9ad307fb3138e1f5ab9daa513a
MakoveiMiron <113377612+MakoveiMiron@users.noreply.github.com>
2025-05-15 10:24:45 +0200

    Update script.js

--- script.js
+++ script.js
@@ -12,9 +12,6 @@ document.querySelectorAll('.skill-circle').forEach(skill => {
     skill.style.setProperty('--skill-level', skillLevel);
 });
 
-
-//emailjs.init('ZIDCvq_z1aaxYXD13');
-
  //emailjs.init({publicKey: "ZIDCvq_z1aaxYXD13"});
 
 document.getElementById('contact-form').addEventListener('submit', function(event) {
