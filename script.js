87b25c9f8d09824e235e3c5bfb8a144a9e94f39a
MakoveiMiron <113377612+MakoveiMiron@users.noreply.github.com>
2025-05-15 08:46:04 +0200

    Update script.js

--- script.js
+++ script.js
@@ -12,9 +12,9 @@ document.querySelectorAll('.skill-circle').forEach(skill => {
     skill.style.setProperty('--skill-level', skillLevel);
 });
 
-//emailjs.init('ZIDCvq_z1aaxYXD13');
+emailjs.init('ZIDCvq_z1aaxYXD13');
 
- emailjs.init({publicKey: "ZIDCvq_z1aaxYXD13"});
+ //emailjs.init({publicKey: "ZIDCvq_z1aaxYXD13"});
 
 document.getElementById('contact-form').addEventListener('submit', function(event) {
     event.preventDefault();
